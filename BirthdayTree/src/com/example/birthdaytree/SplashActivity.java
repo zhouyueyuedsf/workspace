98d471881bb578dbdc9fcea51e37fa1449a78cc1
zhouyueyuedsf <18716833534@163.com>
2015-02-16 17:45:26 +0800

    安卓项目

---
+++ BirthdayTree/src/com/example/birthdaytree/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.birthdaytree;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public void setContentView() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.splash_main);
+	}
+
+	@Override
+	public void initViews() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+}
